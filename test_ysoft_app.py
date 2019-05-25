d6a1f1194656f1390aa19ac8c4b44ade7e290fc5
Adam Radvan <xradvan@fi.muni.cz>
2019-05-25 11:55:23 +0200

    finished tests

--- test_ysoft_app.py
+++ test_ysoft_app.py
@@ -1,7 +1,7 @@
 import unittest
+import json
 from ysoft_app import get_lower_ascii_data
-from ysoft_app import parse_arguments
-from ysoft_app import parse_data_from_file
+from ysoft_app import validate_arguments
 from ysoft_app import write_json_file
 
 
@@ -31,3 +31,66 @@ class TestGetLowerAsciiData(unittest.TestCase):
         expected = ""
         self.assertEqual(get_lower_ascii_data(data), expected)
 
+
+class TestValidateArguments(unittest.TestCase):
+    def setUp(self):
+        self.user = "test UserName"
+        self.printer = "test PrinterName"
+        self.input_file = "test_input.txt"
+        self.output_file = "test_output.json"
+
+    def test_ok_arguments(self):
+        try:
+            validate_arguments(self.user, self.printer, self.input_file, self.output_file)
+        except SystemExit:
+            self.fail("Function validate_arguments() failed despite providing correct arguments")
+
+    def test_nok_output_file_not_JSON(self):
+        with self.assertRaises(SystemExit):
+            self.output_file = "test_output.txt"
+            validate_arguments(self.user, self.printer, self.input_file, self.output_file)
+
+    def test_nok_user_name_empty(self):
+        with self.assertRaises(SystemExit):
+            self.user = ""
+            validate_arguments(self.user, self.printer, self.input_file, self.output_file)
+
+    def test_nok_printer_name_empty(self):
+        with self.assertRaises(SystemExit):
+            self.printer = ""
+            validate_arguments(self.user, self.printer, self.input_file, self.output_file)
+
+    def test_nok_invalid_input_path(self):
+        with self.assertRaises(SystemExit):
+            self.input_file = "C:"
+            validate_arguments(self.user, self.printer, self.input_file, self.output_file)
+
+    def test_nok_invalid_output_path(self):
+        with self.assertRaises(SystemExit):
+            self.output_file = "C:"
+            validate_arguments(self.user, self.printer, self.input_file, self.output_file)
+
+
+class TestWriteJSONFile(unittest.TestCase):
+    def setUp(self):
+        self.user = "test UserName"
+        self.printer = "test PrinterName"
+        self.output_file = "test_output.json"
+        self.data = "This is test data."
+
+    def test_write_json_file(self):
+        write_json_file(self.user, self.printer, self.output_file, self.data)
+        with open(self.output_file) as f:
+            json_from_file = json.load(f)
+            self.assertEqual(self.user, json_from_file['userName'])
+            self.assertEqual(self.printer, json_from_file['printerName'])
+            self.assertEqual(self.data, json_from_file['data'])
+
+    def test_write_json_file_empty_data(self):
+        self.data = ""
+        write_json_file(self.user, self.printer, self.output_file, self.data)
+        with open(self.output_file) as f:
+            json_from_file = json.load(f)
+            self.assertEqual(self.user, json_from_file['userName'])
+            self.assertEqual(self.printer, json_from_file['printerName'])
+            self.assertEqual(self.data, json_from_file['data'])
